0277e2c89ae06e1cb2c10e66309441e757cb1312
Melissa Castorena <mcastorena0316@gmail.com>
2020-07-11 15:15:19 -0500

    add questions and answers module without styling

--- src/templates/details.js
+++ src/templates/details.js
@@ -3,6 +3,7 @@ import {
   Tab, Tabs, TabList, TabPanel,
 } from 'react-tabs';
 import StarRatingComponent from 'react-star-rating-component';
+import ScriptTag from 'react-script-tag';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -68,6 +69,76 @@ const ProductDetails = (data) => (
             }}
           />
         </div>
+      </div>
+      <div>
+        <ScriptTag
+          type="text/javascript"
+          src="https://netlify2.services.answerbase.com/javascript/widget/full-featured-widget.js"
+          data-product-title="Soy un titulo"
+          data-product-id=""
+          data-product-price=""
+          data-product-url=""
+          data-product-description=""
+          data-product-image-url=""
+          data-text-ask="Tienes una pregunta? Busca respuesta.."
+          data-text-button="Publica tu Pregunta"
+          data-text-list-popular="Preguntas mas populares"
+          data-text-list-recent="Preguntas Recientes"
+          data-text-characters-remaining="characters remaining"
+          data-text-empty-list="Nadie ha hecho preguntas aun"
+          data-text-pending-answer="Pending answer submission"
+          data-open-links="popup"
+          data-open-ask="popup"
+          data-list-type="popular"
+          data-display-count="10"
+          data-show-answers="true"
+          data-show-comments="false"
+          data-expand-comments="false"
+          data-max-answer-height="100"
+          data-show-only-micro-content="false"
+          data-view-all="false"
+          data-page-specific-content="false"
+          data-show-question-list="true"
+          data-show-product="false"
+          data-show-category="false"
+          data-page-association="false"
+          data-automatic-updates="true"
+          data-insert-after=""
+        />
+        {/* <script
+        type="text/javascript"
+        src="https://netlify2.services.answerbase.com/javascript/widget/full-featured-widget.js"
+        data-product-id=""
+        data-product-title=""
+        data-product-price=""
+        data-product-url=""
+        data-product-description=""
+        data-product-image-url=""
+        data-text-ask="Ask a question..."
+        data-text-button="Continue"
+        data-text-list-popular="Popular Questions"
+        data-text-list-recent="Recent Questions"
+        data-text-characters-remaining="characters remaining"
+        data-text-empty-list="No questions have been asked yet, ask your question above."
+        data-text-pending-answer="Pending answer submission"
+        data-open-links="popup"
+        data-open-ask="popup"
+        data-list-type="popular"
+        data-display-count="10"
+        data-show-answers="true"
+        data-show-comments="false"
+        data-expand-comments="false"
+        data-max-answer-height="100"
+        data-show-only-micro-content="false"
+        data-view-all="false"
+        data-page-specific-content="false"
+        data-show-question-list="true"
+        data-show-product="false"
+        data-show-category="true"
+        data-page-association="false"
+        data-automatic-updates="true"
+        data-insert-after=""
+      /> */}
       </div>
     </div>
   </Layout>
